893eedac0d1e6d4f4040b81ce6acc7d5e3b7f5b1
Mateusz <m.kacprowicz98@gmail.com>
2020-03-24 14:48:04 +0100

    Piramida gotowa

--- exercises/day4_01.py
+++ exercises/day4_01.py
@@ -21,6 +21,7 @@ list_1 = [
     [4, 5, 6],
     [7, 8, 9]
 ]
+print(list_1)
 import copy
 new_list = copy.deepcopy(list_1)
 print(list_1[1][1])
---
+++ homework/hm_02_piramida.py
@@ -0,0 +1,10 @@
+height = int(input('Podaj wysokosc piramidy: '))
+i = 1
+m = 1
+width = height * 2 - 1
+while i<= height:
+    x = m * '#'
+    y = x.center(width)
+    print(y)
+    i+=1
+    m+=2

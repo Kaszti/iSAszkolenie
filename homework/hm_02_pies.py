949c84be7ecdd4dada6f899fc912e8645ed064e6
Mateusz <m.kacprowicz98@gmail.com>
2020-03-24 16:59:18 +0100

    rozmieniarka gotowa

---
+++ homework/hm_02_pies.py
@@ -0,0 +1,13 @@
+age_h = float(input('Podaj wiek psa w latach ludzkich: '))
+def age_d(age_h):
+    if age_h <= 2 and age_h > 0:
+      age_dog = age_h * 10.5
+      print(f'Wiek psa w psich latach wynosi: {age_dog}')
+    elif age_h > 2:
+        age_dog_1 = 2 * 10.5
+        age_dog_2 = (age_h - 2) * 4
+        age_dog_sum = age_dog_1 + age_dog_2
+        print(f'Wiek psa w psich latach wynosi: {age_dog_sum}')
+age_d(age_h)
+
+

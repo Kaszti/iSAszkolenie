26d0ccf3fd0ce5957c41f2582e8f884560c50980
Mateusz <m.kacprowicz98@gmail.com>
2020-03-25 20:19:28 +0100

    Nakładka ready

--- homework/nakladka.py
+++ homework/nakladka.py
@@ -2,32 +2,20 @@ import hm_01 as hm
 import hm_02_rozmieniarka as rozmieniarka
 import hm_02_piramida as pir
 import hm_02_pies as pies
+import random
+import sys
+from colorama import Fore
 
 def another():
     question = input('Czy wykonac inny program (tak/nie)? ')
     if question == 'nie':
-        print('Wyszedles z aplikacji.')
+        print(Fore.RED + 'Wyszedles z aplikacji.')
+        sys.exit(0)
     elif question == 'tak':
-        main()
-def main():
-    print('Witaj w Multitool Python Program by iSA - wersja B02.12321')
-    print('''Wybierz program ktory chcesz uruchomic
-    1) Przeliczanie C -> F
-    2) Przeliczanie F -> C
-    3) Obliczanie pola powierzchni kola
-    4) Sprawdzanie pierwszej i ostatniej cyfry
-    5) Rysowanie prostokata
-    6) Przeliczanie liczby zapisanej w formacie binarnym na dziesietny
-    7) Rozpoznawanie parzystosci
-    8) Podzielnosc przez 3 lub 5 lub 7
-    9) Podzielnosc przez 3 i 5 i 7
-    10) Sprawdzanie przestępczości roku
-    11) Rozmieniarka
-    12) Rysowanie piramidy
-    13) Kalkulator wieku psa
-    R) Wybierz program losowo
-    X) Wyjście z programu''')
-    choice = input('Twoj wybor: ')
+        intro()
+        choice = input('Twoj wybor: ')
+        main(choice)
+def main(choice):
     if choice == '1':
         hm.cf()
         another()
@@ -70,7 +58,32 @@ def main():
         pies.age_d(age_h)
         another()
     elif choice == 'R' or choice == 'r':
-        pass
+        r_choice_n = random.randint(0, 14)
+        r_choice_S = str(r_choice_n)
+        print(f'Wylosowales program o numerze: {r_choice_S}')
+        main(r_choice_S)
+        another()
     elif choice == 'X' or choice == 'x':
-        print('Wyszedles z aplikacji.')
-main()
+        print(Fore.RED + 'Wyszedles z aplikacji.')
+        sys.exit(0)
+def intro():
+    print(Fore.WHITE + '''Wybierz program ktory chcesz uruchomic
+1) Przeliczanie C -> F
+2) Przeliczanie F -> C
+3) Obliczanie pola powierzchni kola
+4) Sprawdzanie pierwszej i ostatniej cyfry
+5) Rysowanie prostokata
+6) Przeliczanie liczby zapisanej w formacie binarnym na dziesietny
+7) Rozpoznawanie parzystosci
+8) Podzielnosc przez 3 lub 5 lub 7
+9) Podzielnosc przez 3 i 5 i 7
+10) Sprawdzanie przestępczości roku
+11) Rozmieniarka
+12) Rysowanie piramidy
+13) Kalkulator wieku psa
+R) Wybierz program losowo
+X) Wyjście z programu''')
+print(Fore.BLUE + 'Witaj w Multitool Python Program by iSA - wersja B02.12321')
+intro()
+choice = input('Twoj wybor: ')
+main(choice)
